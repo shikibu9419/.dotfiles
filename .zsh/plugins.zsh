05161e00f683325ecd723a78cc9428b790cea372
shikibu9419 <shikibu9419@gmail.com>
2018-03-16 00:48:58 +0900

    Add zplug settings

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -2,14 +2,16 @@ source /usr/local/opt/zplug/init.zsh
 
 zplug "zsh-users/zsh-completions", lazy:true
 zplug "b4b4r07/enhancd", use:init.sh
-zplug "mollifier/cd-gitroot", as:command
-zplug "b4b4r07/zsh-gomi", as:command
-
-# if ! zplug check --verbose; then
-#   printf "Install? [y/N]: "
-#   if read -q; then
-#     echo; zplug install
-#   fi
-# fi
+zplug "mollifier/cd-gitroot", use:'!*'
+zplug "b4b4r07/zsh-gomi", as:command, use:bin
 
 zplug load --verbose
+
+#------------------------------------------
+# Plugins Setting
+#------------------------------------------
+cd-gitroot() {
+  unfunction cd-gitroot
+  . ~/.zplug/repos/mollifier/cd-gitroot/cd-gitroot
+}
+alias cdg=cd-gitroot
