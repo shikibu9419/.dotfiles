3151c5918f454e9b0c8f6fea4b673565e0f7cfc7
shikibu9419 <shikibu9419@gmail.com>
2018-04-13 23:12:58 +0900

    Add zsh-plugins

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,7 +1,9 @@
 source /usr/local/opt/zplug/init.zsh
 
 zplug "zsh-users/zsh-completions", lazy:true
+zplug "zsh-users/zsh-syntax-highlighting"
 zplug "b4b4r07/enhancd", use:init.sh
+zplug "b4b4r07/emoji-cli"
 zplug "mollifier/cd-gitroot", use:'!*'
 zplug "b4b4r07/zsh-gomi", as:command, use:bin
 
