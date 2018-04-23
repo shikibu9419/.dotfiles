258f89bf6f5564027291e11789594830229de0cd
shikibu9419 <shikibu9419@gmail.com>
2018-04-24 00:02:27 +0900

    4月24日はalias g='git'記念日

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,11 +1,27 @@
-alias vim='nvim'
+# ls
 alias la='ls -a'
 alias ll='ls -l'
 alias lla='ls -al'
 alias lal='ls -al'
+
+# color
 alias ls='gls --color=auto'
 alias grep='grep --color=auto'
-alias gti='git'
+
+# git
+alias g='git'
+alias ga='git add'
+alias gb='git branch'
+alias gc='git commit -m'
+alias gca='git commit --amend'
+alias gch='git checkout'
+alias gp='git push'
+alias gpl='git pull --rebase'
+alias grb='git rebase'
+alias grs='git reset'
+alias gs="git status"
+
+alias vim='nvim'
 alias rm='rm -r'
 alias cp='cp -r'
 alias mkdir='mkdir -p'
