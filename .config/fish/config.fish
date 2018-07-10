dc6c3e1154825fbb98fc1f926a955a7658dc4e4f
shikibu9419 <shikibu9419@gmail.com>
2018-07-11 00:09:46 +0900

    Return xdg_configs/* to .config/

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,28 +2,38 @@ if [ -z $TMUX ]
   tmux; and exit
 end
 
+# TODO
+# pip completion
+# emoji?, enhancd, k?, tmuximum to fish?
+# k() {
+#   unfunction k
+#   . ~/.zplug/repos/supercrabtree/k/k.sh
+#
+#   k "$@"
+# }
+
 source ~/.config/fish/env.fish
 
-function fish_user_key_bindings
-  bind \cr __peco_select_history
-  bind \c] __peco_ghq
+function fish_right_prompt
 end
 
+function fish_user_key_bindings
+  # bind \cr __peco_select_history
+  bind \c] __ghq_project_search
+end
+
+# function add_history
+#   さっきのコマンドが5文字未満 or コマンドがexitじゃなかったらdelete?
+# end
+
+# key bindings
 for mode in insert default visual
   fish_default_key_bindings -M $mode
 end
 
 fish_vi_key_bindings --no-erase
 
-
-# set fish_pager_color_completion --bold
-# set fish_pager_color_secondary
-
-# set bg_color = secondary ? highlight_spec_pager_secondary : highlight_spec_normal
-set -g theme_display_user yes
-# set -g theme_hostname never
-# set -g theme_hostname always
-set -g default_user your_normal_user
+set fish_pager_color_completion green --bold
 
 
 #------------------------------------------
@@ -33,15 +43,15 @@ balias reload 'source ~/.config/fish/config.fish'
 balias fishrc 'vim ~/.config/fish/config.fish; and reload'
 balias ls     'gls --color'
 balias grep   'grep --color'
+balias git  'hub'
 
 # git
-abbr git  'hub'
 abbr g    'git'
 abbr ga   'git add'
 abbr gb   'git branch'
 abbr gc   'git commit -m'
 abbr gca  'git commit --amend'
-abbr gcl  'git-clone'
+abbr gcl  'git clone'
 abbr gch  'git checkout'
 abbr gd   'git diff'
 abbr gl   'git log'
--- .config/fish/functions/__ghq_project_search.fish
+++ .config/fish/functions/__ghq_project_search.fish
@@ -1,8 +1,8 @@
-function __peco_ghq
+function __ghq_project_search
   commandline | read -l buffer
   ghq list --full-path | \
     sed -e "s|$HOME/||g" | \
-    peco | \
+    fzf | \
     read -l repository_path
   if [ -n "$repository_path" ]
     commandline "cd ~/$repository_path"
