c2bd7cacc3f8fdcf26c2f51b802862378f71e2a0
shikibu9419 <shikibu9419@gmail.com>
2018-08-07 00:16:54 +0900

    Add DOTPATH to environment

--- .fish/env.fish
+++ .fish/env.fish
@@ -2,5 +2,6 @@ set -x LANG ja_JP.utf-8
 set -x LC_ALL ja_JP.utf-8
 set -x TERM xterm-256color
 set -x EDITOR vim
+set -x DOTPATH ~/dotfiles
 set -x XDG_CONFIG_HOME ~/.config
-set -x PATH ~/dotfiles/bin $PATH
+set -x PATH $DOTPATH/bin $PATH
