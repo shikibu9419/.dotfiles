303787f7653ea524ea100efc84ea63d304c01635
Kazuya Izumi <shikibu9419@gmail.com>
2018-12-12 22:19:35 +0900

    Fix and update zsh functions and my scripts

--- zsh/40-keybinds.zsh
+++ zsh/40-keybinds.zsh
@@ -4,18 +4,18 @@ autoload -Uz zmv
 autoload -U history-search-end
 autoload -U tetris; zle -N tetris
 
-zle -N git_list_checkout
+zle -N git_list_branch
 zle -N _show_ls_gs
 zle -N select_history
-zle -N ghq_jump_repositories
+zle -N ghq_jump_repository
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 
 bindkey -v
-bindkey '^b' git_list_checkout
+bindkey '^b' git_list_branch
 bindkey '^m' _show_ls_gs
 bindkey '^r' select_history
-bindkey '^]' ghq_jump_repositories
+bindkey '^]' ghq_jump_repository
 bindkey '^e' autosuggest-accept
 bindkey '^t' forward-word  # autosuggest-partial-accept
 bindkey '^P' history-beginning-search-backward-end
--- zsh/60-functions.zsh
+++ zsh/60-functions.zsh
@@ -9,7 +9,7 @@ _show_ls_gs() {
   ls
   echo
 
-  if $(git rev-parse > /dev/null 2>&1); then
+  if _check_available_git; then
     echo -e '\e[0;33m--- git status ---\e[0m'
     git status -sb
     echo
@@ -22,4 +22,62 @@ _show_ls_gs() {
 select_history() {
   local BUFFER=$(fc -l -n 1 | fzf --reverse --tac --query=$LBUFFER)
   CURSOR=$#BUFFER
+}
+
+git_list_branch() {
+  if ! _check_available_git; then
+    echo 'fatal: not a git repository.'
+    return 1
+  fi
+
+  branch=$(git branch --all | grep -v HEAD | fzf-tmux --ansi -d)
+  [ -n $branch ] && git checkout $(echo $branch | sed 's/.* //' | sed 's#remotes/[^/]*/##')
+  zle reset-prompt
+}
+
+ghq_jump_repository() {
+  local selected dir repo session current_session
+  selected=$(ghq list | fzf --query=$LBUFFER)
+  dir=$(ghq root)/$selected
+
+  if [ -z $selected ]; then
+    return
+  elif [ -z $TMUX ]; then
+    BUFFER="cd $dir"
+    zle accept-line
+    return
+  fi
+
+  repo=${dir##*/}
+  if [[ ! $selected =~ ^github\.com.+$ ]]; then
+    parent=${dir%/*}
+    repo=${parent##*/}/$repo
+  fi
+
+  session=${repo//./-}
+  current_session=$(tmux list-sessions | grep attached | cut -d":" -f1)
+
+  if [[ $current_session = $session ]]; then
+    BUFFER="cd $dir"
+    zle accept-line
+    return
+  fi
+
+  if [[ -n $(tmux list-sessions | grep $session) ]]; then
+    tmux switch-client -t $session
+    return
+  fi
+
+  if [[ $current_session =~ ^[0-9]+$ ]]; then
+    BUFFER="cd $dir"
+    zle accept-line
+    tmux rename-session $session
+  else
+    tmux new-session -d -c $dir -s $session
+    tmux switch-client -t $session
+  fi
+}
+
+_check_available_git() {
+  git rev-parse > /dev/null 2>&1
 }
