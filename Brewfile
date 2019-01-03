65260d8f8f25eb92f1c1adeb90143e917a47f97e
Kazuya Izumi <shikibu9419@gmail.com>
2019-01-03 15:55:23 +0900

    Change initialize to minimal

--- Brewfile
+++ Brewfile
@@ -6,123 +6,42 @@ cask_args appdir: '/Applications'
 tap 'homebrew/core'
 tap 'homebrew/services'
 tap 'caskroom/cask'
-tap 'caskroom/fonts'
 tap 'caskroom/versions'
-tap 'sanemat/font'
-tap 'kyoshidajp/ghkw'
 
 #------------------------------------------
 # brew
 #------------------------------------------
 brew 'coreutils'
-brew 'mas'
-
 brew 'zsh'
-brew 'zsh-completions'
-brew 'zplug'
-brew 'ghkw'
-brew 'fzf'
-brew 'emojify'
-brew 'jq'
-brew 'fpp'
 brew 'fish'
 brew 'xonsh'
-
+brew 'zsh-completions'
+brew 'zplug'
 brew 'git'
-brew 'tig'
 brew 'hub'
 brew 'ghq'
-brew 'peco'
-brew 'bat'
-brew 'tree'
-
 brew 'vim', args: ['with-lua']
 brew 'neovim'
 brew 'ctags'
 brew 'global', args: ['with-exuberant-ctags', 'with-pygments']
 brew 'tmux'
 brew 'reattach-to-user-namespace'
-# cask 'xquartz' # dependency
-# brew 'gnuplot', args: ['with-x11']
-
+brew 'python'
+brew 'python@2'
 brew 'ruby-build'
 brew 'rbenv'
 brew 'kotlin'
 brew 'go'
-brew 'lua'
-brew 'awk'
-brew 'gradle'
-brew 'gradle-completion'
-
-brew 'unicorn'
-brew 'nginx'
-brew 'redis'
-brew 'sqlite'
-brew 'postgresql'
-
-brew 'yarn'
-brew 'node'
-brew 'llvm'
-brew 'cmake'
-brew 'imagemagick@6'
-brew 'graphviz'
-brew 'readline'
-brew 'openssl'
-
-brew 'highlight'
-brew 'httpd'
-brew 'pandoc'
-brew 'cowsay'
-brew 'nmap'
-brew 'easy-tag'
-brew 'wget'
 
 #------------------------------------------
 # cask
 #------------------------------------------
-# cask 'station'
 cask 'google-chrome'
 cask 'firefox'
-# cask 'mactex'
 cask 'docker'
 cask 'docker-toolbox'
-
 cask 'alacritty'
-# cask 'iterm2'
 cask 'hyper'
-cask 'cool-retro-term'
-
+cask 'visual-studio-code'
 cask 'dash'
 cask 'alfred'
-# cask 'cheatsheet'
-cask 'appcleaner'
-cask 'bettertouchtool'
-cask 'skim'
-
-cask 'sublime-text'
-cask 'atom'
-cask 'visual-studio-code'
-cask 'brackets'
-cask 'android-studio'
-cask 'intellij-idea-ce'
-cask 'pycharm-ce'
-
-cask 'java'
-cask 'androidtool'
-cask 'android-platform-tools'
-
-cask 'adobe-creative-cloud'
-cask 'postman'
-cask 'mendeley'
-# cask 'quail'
-cask 'tunnelblick'
-cask 'skype'
-cask 'zoomus'
-cask 'notion'
-# cask 'kindle'
-cask '4k-video-downloader'
-cask 'xld'
-
-cask '0xed'
-cask 'wireshark'
-cask 'netspot'
--- etc/init/initialize.sh
+++ etc/init/initialize.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 set -e
-DOCKER_COMPLETIONS_PATH=/Applications/Docker.app/Contents/Resources/etc
-ZSH_COMPLETIONS_PATH=~/.zsh/completions
-RBENV_DEFAULT_GEMS=https://github.com/sstephenson/rbenv-default-gems.git
 DEFAULT_GEMS_URL=https://gist.githubusercontent.com/shikibu9419/c4a6e126b0da47856ee5a8ef04d38cde/raw/rbenv-default-gems
 PIP_REQUIREMENTS_URL=https://gist.githubusercontent.com/shikibu9419/8bafc4e6146967d851d0f88567f9d15a/raw/pip-requirements
+DOCKER_COMPLETIONS_PATH=/Applications/Docker.app/Contents/Resources/etc
+ZSH_COMPLETIONS_PATH=~/.zsh/completions
 
 has() {
   return $(type "$1" > /dev/null 2>&1)
@@ -39,16 +38,13 @@ checkdir() {
 
 cd $DOTPATH
 
-## brew install
-notice 'Start install formulas.'
+notice 'Install formulas.'
 brew tap homebrew/bundle
 brew bundle
 brew cleanup
 strong 'Installation of formulas finished!\n'
 
-notice 'Other settings...'
-
-## Shell
+notice 'Formula settings...'
 strong 'Shell:'
 for shell in (z|fi|xon)sh; do
   if has 'tee'; then
@@ -59,13 +55,14 @@ for shell in (z|fi|xon)sh; do
 done
 chsh -s $(which zsh)
 
-## Editor
+strong 'Global:'
+sed -e 's/:tc=native:/:tc=native:tc=pygments:/g' /usr/local/opt/global/share/gtags/gtags.conf > ~/.globalrc
+
 strong 'VS Code:'
 cat $DOTPATH/etc/init/vscode-extensions | while read pkg; do
   code --install-extension $pkg
 done
 
-## Docker
 strong 'Docker:'
 checkdir $ZSH_COMPLETIONS_PATH
 for comp in $DOCKER_COMPLETIONS_PATH/*.zsh-completion; do
@@ -73,23 +70,20 @@ for comp in $DOCKER_COMPLETIONS_PATH/*.zsh-completion; do
   cp $comp $ZSH_COMPLETIONS_PATH/_${file%.*}
 done
 
-## Package managers
 strong 'gem:'
 checkdir ~/.rbenv
-git clone $RBENV_DEFAULT_GEMS ~/.rbenv/plugins/rbenv-default-gems
+git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
 curl -fsSL $DEFAULT_GEMS_URL -o default-gems && mv default-gems ~/.rbenv
 
 strong 'pip:'
 pip3 install --upgrade setuptools
 pip3 install $(curl -fsSL $PIP_REQUIREMENTS_URL)
 
-## Others
-sed -e 's/:tc=native:/:tc=native:tc=pygments:/g' /usr/local/opt/global/share/gtags/gtags.conf > ~/.globalrc
-
 
 strong 'Initialization finished successfully!!'
 cat <<EOF
 Please run this commands to complete initialize truly.
+> zplug install
 > curl https://sh.rustup.rs -sSf | sh
 > rustup component add rls-preview rust-analysis rust-src
 EOF
